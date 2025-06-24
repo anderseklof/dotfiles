03ba92695a6742aad5b96e5383ef8c3bfc218984
Anders Eklöf <69395876+anderseklof@users.noreply.github.com>
2025-06-24 07:23:26 +0200

    Minor adjustments, added comments

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,13 +1,15 @@
 return {
-	{
-		"folke/noice.nvim",
-		opts = {
-			presets = {
-				bottom_search = false,
-				command_palette = false,
-				long_message_to_split = true,
-				inc_rename = true,
-			},
-		},
-	},
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        -- Disables bottom search
+        bottom_search = false,
+        -- Place search and cmdline on center of screen
+        command_palette = false,
+        -- Long messages will be split
+        long_message_to_split = true,
+      },
+    },
+  },
 }
